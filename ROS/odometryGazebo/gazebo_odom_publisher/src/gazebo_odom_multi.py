a74ea34bcf7cf826c0f7f884d258b95d79df1c0e
yog2104 <yog2104@users.noreply.github.com>
2019-03-06 20:35:56 -0800

    Add files via upload

---
+++ ROS/odometryGazebo/gazebo_odom_publisher/src/gazebo_odom_multi.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Header
+from gazebo_msgs.srv import GetModelState, GetModelStateRequest
+
+rospy.init_node('odom_pub')
+robot_name = rospy.get_param('~robotName')
+
+odom_pub = rospy.Publisher('/%s/gazeboOdom'% robot_name,Odometry,queue_size=10)
+
+rospy.wait_for_service('/gazebo/get_model_state')
+get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state',GetModelState)
+
+odom = Odometry()
+header = Header()
+header.frame_id = '/odom'
+
+model = GetModelStateRequest()
+
+model.model_name = robot_name
+
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+	result = get_model_srv(model)
+	
+	odom.pose.pose = result.pose
+	odom.twist.twist = result.twist
+
+	header.stamp = rospy.Time.now()
+	odom.header = header
+	odom_pub.publish(odom)
+
+	rate.sleep()
